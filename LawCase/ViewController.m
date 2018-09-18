627dbff33676b76504129d072d4b8317144a4091
Affee <304442951@qq.com>
2018-09-18 19:02:02 +0800

    修正问题

--- LawCase/ViewController.m
+++ LawCase/ViewController.m
@@ -16,7 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor yellowColor];
+    // Do any additional setup after loading the view, typically from a nib. shenm wayan
 }
 
 
